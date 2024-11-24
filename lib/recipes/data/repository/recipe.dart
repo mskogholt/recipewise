4e3f2c35afc33770e63151da96815cfdef4a01db
mskogholt <40392061+mskogholt@users.noreply.github.com>
2024-11-24 14:27:52 +0100

    Setup data skeleton

---
+++ lib/recipes/data/repository/recipe.dart
@@ -0,0 +1,2 @@
+// This is the recipe model as it should be used by the application
+// This will be tied to the repository
---
+++ lib/recipes/data/repository/recipe_repository.dart
@@ -0,0 +1,2 @@
+// TODO: I guess this repo also interacts with web scraping apis
+// TODO: How to handle the above?
